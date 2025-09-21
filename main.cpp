400034fb7617265bfaf693998c2f7fc7c7e04d99
Jailior <ahusseinse@yahoo.com>
2025-09-20 20:50:51 -0700

    add mouse click to put block

--- main.cpp
+++ main.cpp
@@ -1,11 +1,8 @@
 // #include "lib/common.h"
 #include "lib/texture_tools.h"
 
+enum Material : uint8_t { EMPTY, SAND, WATER, STONE };
 
-// Each pixel is 0xAARRGGBB (Alpha, Red, Green, Blue)
-std::vector<uint32_t> framebuffer(WIDTH * HEIGHT, 0xFF000000);
-
-enum Material { EMPTY, SAND, WATER, STONE };
 uint32_t palette[] = {
     0xFF000000, // empty = black
     0xFFFFFF00, // sand = yellow
@@ -14,14 +11,62 @@ uint32_t palette[] = {
 };
 
 
-// Example simulation step: random noise
+// Each pixel is 0xAARRGGBB (Alpha, Red, Green, Blue)
+std::vector<uint32_t> framebuffer(WIDTH * HEIGHT, palette[EMPTY]);
+std::vector<Material> grid(WIDTH * HEIGHT, EMPTY);
+
+inline int idx(int x, int y) {
+    return y * WIDTH + x;
+}
+
+inline bool in_bounds(int x, int y) {
+    return x >= 0 && x < WIDTH && y >= 0 && y < HEIGHT;
+}
+
+
+
+
 void stepSimulation() {
+    // Iterate from bottom up so particles don't "fall twice"
+    for (int y = HEIGHT - 2; y >= 0; --y) {
+        for (int x = 0; x < WIDTH; ++x) {
+            int i = idx(x, y);
+            Material m = grid[i];
+            if (m == EMPTY || m == STONE) continue;
+
+            if (m == SAND) {
+                // Try move down
+                if (in_bounds(x, y+1) && grid[idx(x, y+1)] == EMPTY) {
+                    std::swap(grid[i], grid[idx(x, y+1)]);
+                }
+                // Try diagonals
+                else if (in_bounds(x-1, y+1) && grid[idx(x-1, y+1)] == EMPTY) {
+                    std::swap(grid[i], grid[idx(x-1, y+1)]);
+                }
+                else if (in_bounds(x+1, y+1) && grid[idx(x+1, y+1)] == EMPTY) {
+                    std::swap(grid[i], grid[idx(x+1, y+1)]);
+                }
+            }
+            else if (m == WATER) {
+                if (in_bounds(x, y+1) && grid[idx(x, y+1)] == EMPTY) {
+                    std::swap(grid[i], grid[idx(x, y+1)]);
+                }
+                else {
+                    int dir = (rand() & 1) ? -1 : 1; // left or right
+                    if (in_bounds(x+dir, y) && grid[idx(x+dir, y)] == EMPTY) {
+                        std::swap(grid[i], grid[idx(x+dir, y)]);
+                    }
+                }
+            }
+        }
+    }
+
+    // Update framebuffer colors
     for (int i = 0; i < WIDTH * HEIGHT; i++) {
-        if (framebuffer[i] == palette[STONE]) continue; // stone is static
-        uint mat = rand() % 4;
-        framebuffer[i] = palette[mat];
+        framebuffer[i] = palette[grid[i]];
     }
 }
+
 
 
 int main(int argc, char* argv[]) {
@@ -60,6 +105,20 @@ int main(int argc, char* argv[]) {
         while (SDL_PollEvent(&e)) {
             if (e.type == SDL_QUIT) running = false;
             if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) running = false;
+            
+        }
+
+        int mx, my;
+        Uint32 mouse = SDL_GetMouseState(&mx, &my);
+        if (mouse & SDL_BUTTON(SDL_BUTTON_RIGHT)) {
+            int gx = mx * WIDTH / WIN_W;
+            int gy = my * HEIGHT / WIN_H;
+            if (in_bounds(gx, gy)) grid[idx(gx, gy)] = SAND;
+        }
+        if (mouse & SDL_BUTTON(SDL_BUTTON_LEFT)) {
+            int gx = mx * WIDTH / WIN_W;
+            int gy = my * HEIGHT / WIN_H;
+            if (in_bounds(gx, gy)) grid[idx(gx, gy)] = WATER;
         }
 
         stepSimulation();
